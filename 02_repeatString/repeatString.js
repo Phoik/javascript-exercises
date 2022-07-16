4b8ffb7173412779f0835d48886bf1e4985393a7
Jason <Jasonwilsn01@gmail.com>
2022-07-16 15:17:22 +0100

    Complete task 02 of Javascript exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+ let string;
+ let number; 
 
-};
+const repeatString = function(string, number) {
+    
+    if (number < 0){
+        return "ERROR"
+    }
+    let repeatedString = "";
+    while (number > 0) {
+        repeatedString += string;
+        number--;
+    }
+    return repeatedString;
+
+    
+};  
 
 // Do not edit below this line
 module.exports = repeatString;
